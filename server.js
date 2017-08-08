a6d3e4c6539ffcabeeaf625ed35e7179bf5ebb2c
ramR2 <30644956+ramR2@users.noreply.github.com>
2017-08-08 23:06:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,15 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/article-1',function(req,res){
+    res.send("article 1 requested and will be served");
+});
+app.get("/article-2",function(req,res){
+    res.send("article 2 requested and will be served")
+});
+app.get("/article-3",function(req,res){
+    res.send("article 3 requested and will be served")
+});
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
