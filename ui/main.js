e158f446836f7eed9847d942122f5e500ed20933
ramR2 <30644956+ramR2@users.noreply.github.com>
2017-08-13 09:57:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ var img=document.getElementById('madi');
 var marginLeft=0;
 function moveRight(){
     marginLeft=marginLeft+10;
-    img.style.marginLeft=marginLeft+1;
+    img.style.marginLeft=marginLeft+'px';
     
 }
 img.onclick=function() {
