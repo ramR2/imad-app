94c3b66ff51f8f9a791cfa08e951125b30ade4eb
ramR2 <30644956+ramR2@users.noreply.github.com>
2017-09-02 14:35:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,44 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var articleOne={
+    title : 'Articleone',
+    content: `<div class="container"> 
+        <a href="/">Home</a> 
+        <a href="http://ramrajani.imad.hasura-app.io/article-2">jokes</a>
+        <a href="/article-3">end page </a>
+        <hr/>
+                     <div class="center text-big bold">
+           <h2>  R-SQUARE corporation</h2>
+            
+           
+                </div>
+      
+        
+       <div></div> <h2>Founder & CEO -Ram Rajani</h2>
+        <h3>Grow ur business with us:):)</h3>
+        <p>we have hired fb and twitter  :):)</p>
+           </div>`
+    
+
+};
+function createTemplate(data){
+    var title=data.title;
+    var content=data.content;
+var htmltemplate=`<!doctype html>
+<html>
+    <head>
+      <title>${title}</title> 
+      <meta name="viewport" content="width=device-width, initial-scale=1"/>
+      <link href="/ui/style.css" rel="stylesheet"/>
+    </head>
+    <body >
+       ${content}
+    </body>
+</html>`;
+return htmltemplate;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -17,7 +55,7 @@ app.get('/ui/main.js', function (req, res) {
 });
 
 app.get('/article-1',function(req,res){
-  res.sendFile(path.join(__dirname, 'ui', 'article-1.html'));
+  res.sendFile(createTemplate(articleOne));
 });
 app.get("/article-2",function(req,res){
    res.sendFile(path.join(__dirname, 'ui', 'article-2.html'));
